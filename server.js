fba7e3b9f82d2f1c4be6ca98be44f333f3dad895
pericarney00 <pericarney@gmail.com>
2025-07-15 17:00:43 -0500

    Completed step 3

--- server.js
+++ server.js
@@ -21,5 +21,29 @@ app.get("/roll/:number", (req, res) => {
     }
 })
 
+// 3. I want that one
+
+    const collectibles = [
+    { name: "shiny ball", price: 5.95 },
+    { name: "autographed picture of a dog", price: 10 },
+    { name: "vintage 1970s yogurt SOLD AS-IS", price: 0.99 },
+    ];
+
+app.get("/collectibles/:index", (req, res) => {
+    let index = Number(req.params.index)
+    if (!collectibles[index]) {
+        res.send("This item is not yet in stock")
+    } else {
+        res.send(`So, you want the ${collectibles[index].name} for ${collectibles[index].price} correct?`)
+}
+})
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {})
